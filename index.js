440f9992804e919d93212f791aa7b833df295bc7
sharifalmahmudjamil <smjamil111@gmail.com>
2023-11-21 15:35:37 -1200

    payment intent and payment history send the database

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
     const menuCollection= client.db("bistroDb").collection("menu");
     const reviewCollection= client.db("bistroDb").collection("reviews");
     const cartCollection= client.db("bistroDb").collection("carts");
+    const paymentCollection= client.db("bistroDb").collection("payments");
       
       // jwt related api
       app.post('/jwt',async(req,res)=>{
@@ -212,6 +213,38 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret
       })
+    })
+
+    // payment related api
+
+    app.get('/payments/:email',verifyToken,async(req,res)=>{
+      const query={email:req.params.email};
+      if(req.params.email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const result= await paymentCollection.find(query).toArray();
+      res.send(result);
+    })
+
+
+
+
+
+
+    app.post('/payments',async(req,res)=>{
+      const payment=req.body;
+      const paymentResult= await paymentCollection.insertOne(payment);
+      console.log('payment info',payment);
+        // carefully delete each item form the card
+        const query ={ _id:{
+          $in: payment.cartIds.map(id=> new ObjectId(id))
+        }}
+
+        const deleteResult= await cartCollection.deleteMany(query);
+
+        res.send({paymentResult,deleteResult});
+
+
     })
 
     // Send a ping to confirm a successful connection
